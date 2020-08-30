00f5aad370d766735035905f1f631f855ec7f2d1
UbadahJ <ubadahjafry@gmail.com>
2020-08-30 17:53:30 +0500

    Added notification support

--- app/src/main/java/com/ubadahj/qidianundergroud/MainActivity.kt
+++ app/src/main/java/com/ubadahj/qidianundergroud/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ActivityNavigator
 import androidx.navigation.findNavController
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.ubadahj.qidianundergroud.databinding.MainActivityBinding
+import com.ubadahj.qidianundergroud.services.NotificationWorker
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private val notificationRequest =
+        PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.HOURS).build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        WorkManager.getInstance(applicationContext).enqueue(notificationRequest)
+//        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+//            "NotificationService", ExistingPeriodicWorkPolicy.KEEP, notificationRequest
+//        )
     }
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
