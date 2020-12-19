fb46d8a710be9620cf86ff51918ab306377340bd
LordChadiwala <ubadahjafry@gmail.com>
2020-12-19 16:36:01 +0500

    Updated getHtml extension to use suspend wrapper for better performance

--- app/src/main/java/com/ubadahj/qidianundergroud/utils/WebViewExtensions.kt
+++ app/src/main/java/com/ubadahj/qidianundergroud/utils/WebViewExtensions.kt
@@ -1,29 +1,28 @@
 package com.ubadahj.qidianundergroud.utils
 
 import android.webkit.WebView
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
-suspend fun WebView.getHtml(): String {
-    var contents: String? = null
+suspend fun WebView.getHtml(): String = suspendCancellableCoroutine { continuation ->
     if (!settings.javaScriptEnabled)
         throw IllegalStateException("Javascript is disabled")
 
     evaluateJavascript(
-        "(function() {\n" +
-                "    return (\n" +
-                "        '<html>' +\n" +
-                "        document.getElementsByTagName('html')[0].innerHTML +\n" +
-                "        '</html>'\n" +
-                "    );\n" +
-                "})();"
-    ) { contents = it }
-
-    while (contents == null)
-        delay(200)
-
-    return contents!!.replace("\\u003C", "<")
-        .replace("\\n", "")
-        .replace("\\t", "")
-        .replace("\\\"", "\"")
-        .replace("<hr />", "")
+            "(function() {\n" +
+                    "    return (\n" +
+                    "        '<html>' +\n" +
+                    "        document.getElementsByTagName('html')[0].innerHTML +\n" +
+                    "        '</html>'\n" +
+                    "    );\n" +
+                    "})();"
+    ) {
+        continuation.resume(
+                it!!.replace("\\u003C", "<")
+                        .replace("\\n", "")
+                        .replace("\\t", "")
+                        .replace("\\\"", "\"")
+                        .replace("<hr />", "")
+        )
+    }
 }
