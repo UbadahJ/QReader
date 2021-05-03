634922f4af1e7933bf3a8b2704114838dbdee61d
LordChadiwala <ubadahjafry@gmail.com>
2021-05-03 06:45:11 +0500

    Update column name from GroupId to GroupLink

--- app/src/main/java/com/ubadahj/qidianundergroud/repositories/ChapterGroupRepository.kt
+++ app/src/main/java/com/ubadahj/qidianundergroud/repositories/ChapterGroupRepository.kt
@@ -32,7 +32,7 @@ class ChapterGroupRepository(context: Context) {
 
     fun isDownloaded(group: ChapterGroup): Boolean =
         database.chapterQueries
-            .getByGroupId(group.link)
+            .getByGroupLink(group.link)
             .executeAsList()
             .size == group.total
 
@@ -60,7 +60,7 @@ class ChapterGroupRepository(context: Context) {
                     val remoteGroup = remoteChapters[group.firstChapter]!!
                     // We need to delete all the previous chapters to make sure foreign key
                     // doesn't fail
-                    database.chapterQueries.deleteByGroupId(group.link);
+                    database.chapterQueries.deleteByGroupLink(group.link);
                     database.chapterGroupQueries.update(
                         link = group.link,
                         updatedText = remoteGroup.text,
