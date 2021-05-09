4014db5db322ea747fab3aa6320dadb4d4576189
LordChadiwala <ubadahjafry@gmail.com>
2021-05-09 16:00:31 +0500

    Add basic caching

--- app/src/main/java/com/ubadahj/qidianundergroud/repositories/MetadataRepository.kt
+++ app/src/main/java/com/ubadahj/qidianundergroud/repositories/MetadataRepository.kt
@@ -1,6 +1,7 @@
 package com.ubadahj.qidianundergroud.repositories
 
 import android.content.Context
+import com.github.ajalt.timberkt.Timber.d
 import com.ubadahj.qidianundergroud.api.WebNovelApi
 import com.ubadahj.qidianundergroud.api.models.webnovel.WNBookRemote
 import com.ubadahj.qidianundergroud.models.Book
@@ -8,8 +9,16 @@ import com.ubadahj.qidianundergroud.models.Metadata
 
 class MetadataRepository(private val context: Context) {
 
+    companion object {
+        private val cache: MutableMap<String, Metadata?> = mutableMapOf()
+    }
+
     suspend fun getBook(book: Book): Metadata? =
-        WebNovelApi.getBook(book)?.toMetadata(book)
+        cache.getOrPut(book.id) {
+            WebNovelApi.getBook(book)?.toMetadata(book).also {
+                d { "$it" }
+            }
+        }
 
     private fun WNBookRemote.toMetadata(book: Book): Metadata =
         Metadata(id, book.id, link, author, coverLink, category, description)
