89f18e6f74cdfb08c61470a097f5df3ab46ff6fb
LordChadiwala <ubadahjafry@gmail.com>
2021-08-24 02:17:10 +0500

    Add support for gestures to change current chapter

---
+++ app/src/main/java/com/ubadahj/qidianundergroud/ui/listeners/OnSwipeTouchListener.kt
@@ -0,0 +1,56 @@
+package com.ubadahj.qidianundergroud.ui.listeners
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import kotlin.math.abs
+
+private const val SWIPE_DISTANCE_THRESHOLD = 160
+private const val SWIPE_VELOCITY_THRESHOLD = 160
+
+/**
+ * Detects left and right swipes across a view.
+ */
+open class OnSwipeTouchListener(context: Context) : OnTouchListener {
+    private val gestureDetector: GestureDetector
+
+    init {
+        gestureDetector = GestureDetector(context, GestureListener())
+    }
+
+    open fun onSwipeLeft() {}
+    open fun onSwipeRight() {}
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        v?.performClick()
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : SimpleOnGestureListener() {
+        override fun onDown(e: MotionEvent): Boolean {
+            return true
+        }
+
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            val distanceX = e2.x - e1.x
+            val distanceY = e2.y - e1.y
+            if (abs(distanceX) > abs(distanceY) &&
+                abs(distanceX) > SWIPE_DISTANCE_THRESHOLD &&
+                abs(velocityX) > SWIPE_VELOCITY_THRESHOLD
+            ) {
+                if (distanceX > 0) onSwipeRight() else onSwipeLeft()
+                return true
+            }
+            return false
+        }
+    }
+
+}
